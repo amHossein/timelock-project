1c7954b46b97c351a55fa01469fbafa5a4c0be88
amHossein <hosseinhastam1999@gmail.com>
2025-11-25 22:01:42 +0330

    add widgets QLineEdit
    widgets to hold timer digits
    
    not function yet

--- src/timelock/ui/main_window.py
+++ src/timelock/ui/main_window.py
@@ -1,6 +1,6 @@
 from PySide6.QtWidgets import *
-from PySide6.QtCore import QSize, Qt, QPoint
-from PySide6.QtGui import QShortcut, QKeySequence, QScreen
+from PySide6.QtCore import Qt, QPoint
+from PySide6.QtGui import QShortcut, QKeySequence, QIntValidator
 from timelock.ui import constants
 from timelock.utils import load
 from timelock.signals import signals
@@ -12,7 +12,6 @@ class MainWindow(QMainWindow):
         self.setWindowFlag(Qt.FramelessWindowHint)
         self.setWindowFlag(Qt.WindowStaysOnTopHint, True)
         self.setGeometry(constants.WINDOW_X,constants.WINDOW_Y,constants.WINDOW_WIDTH, constants.WINDOW_HEIGHT)
-        #self.setFixedSize(QSize(constants.WINDOW_WIDTH,constants.WINDOW_HEIGHT))
         
         self.alignment_center = Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter
         
@@ -63,15 +62,49 @@ class MainWindow(QMainWindow):
             posy = screen.bottom() - height
         
         self.move(posx,posy)
+    
+    def setup_widgets_entry(self, parent:QLabel):
+        parent.WLineEditS = QLineEdit(parent)
+        parent.WLineEditS.resize(constants.WENTRY_WIDTH,constants.WENTRY_HEIGHT)
+        parent.WLineEditS.move(constants.WENTRY_S_POSX,constants.WENTRY_POSY)
         
+        parent.WLineEditSS = QLineEdit(parent)
+        parent.WLineEditSS.resize(constants.WENTRY_WIDTH,constants.WENTRY_HEIGHT)
+        parent.WLineEditSS.move(constants.WENTRY_SS_POSX,constants.WENTRY_POSY)
+        
+        
+        parent.WLineEditM = QLineEdit(parent)
+        parent.WLineEditM.resize(constants.WENTRY_WIDTH,constants.WENTRY_HEIGHT)
+        parent.WLineEditM.move(constants.WENTRY_M_POSX,constants.WENTRY_POSY)
+        
+        parent.WLineEditMM = QLineEdit(parent)
+        parent.WLineEditMM.resize(constants.WENTRY_WIDTH,constants.WENTRY_HEIGHT)
+        parent.WLineEditMM.move(constants.WENTRY_MM_POSX,constants.WENTRY_POSY)
+        
+        
+        parent.WLineEditH = QLineEdit(parent)
+        parent.WLineEditH.resize(constants.WENTRY_WIDTH,constants.WENTRY_HEIGHT)
+        parent.WLineEditH.move(constants.WENTRY_H_POSX,constants.WENTRY_POSY)
+        
+        parent.WLineEditHH = QLineEdit(parent)
+        parent.WLineEditHH.resize(constants.WENTRY_WIDTH,constants.WENTRY_HEIGHT)
+        parent.WLineEditHH.move(constants.WENTRY_HH_POSX,constants.WENTRY_POSY)
+        
+        self.widgets_entries = [parent.WLineEditS, parent.WLineEditSS, parent.WLineEditM, parent.WLineEditMM, parent.WLineEditH, parent.WLineEditHH]
+
+        
+
             
     def setup_ui(self):
-        centeral_widget = QLabel()
-        centeral_widget.setPixmap(load.load_image("ui-f1.png"))
-        centeral_widget.setAlignment(self.alignment_center)
-        self.setCentralWidget(centeral_widget)
-    
-    
+        background = QLabel()
+        background.setPixmap(load.load_image("ui-f1.png"))
+        background.setAlignment(self.alignment_center)
+        self.setCentralWidget(background)
+        
+        self.setup_widgets_entry(background)
+
+        
+        
     def setup_shortcuts(self):
         self.shortcut_quit = QShortcut(QKeySequence("Ctrl+Q"), self)
     
